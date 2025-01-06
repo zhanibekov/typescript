e98d6d3d6bfccff358328bd306a7cb1e10b8fbd4
yelaman <67456840+zhanibekov@users.noreply.github.com>
2025-01-06 15:53:57 +0500

    first commit

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+// let data1: any = 42;
+// data: 'string';
+// data: true
+
+// console.log(data1)
+
+
+let data: any = 42;  // Изначальное значение - 42
+data = 'string';      // Присваиваем новое значение - строку
+data = true;          // Присваиваем новое значение - булевое значение
+
+console.log(data);    // Выведет true
