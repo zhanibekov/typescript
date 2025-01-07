c242483cf2d60c36b1698697b0f46e8c0a5c826b
yelaman <67456840+zhanibekov@users.noreply.github.com>
2025-01-07 10:42:51 +0500

    second commit

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,46 @@
+//1.
 // let data1: any = 42;
 // data: 'string';
 // data: true
 
 // console.log(data1)
 
+//2.
+// let age: number = 25;
+// let userName: string = 'Elaman';
+// let isStudent: boolean = true
+// console.log(age, userName, isStudent)
 
-let data: any = 42;  // Изначальное значение - 42
-data = 'string';      // Присваиваем новое значение - строку
-data = true;          // Присваиваем новое значение - булевое значение
+//3.
+ 
+// const words : string = 'yelaman';
+// const result = words.toUpperCase();
+// console.log(result)
 
-console.log(data);    // Выведет true
+
+//4.
+// const numbers: number[] = [4, 5, 6, 9, 10, -1, -2];
+
+// const result = numbers.filter((num) => num > 0);
+// console.log(result)
+
+
+///5.
+// class Person {
+//     userName: string;
+//     surName: string;
+//     year: number;
+
+// constructor(userName: string, surName: string, year: number) {
+//         this.userName = userName;
+//         this.surName = surName;
+//         this.year = year;
+// }
+
+// getPersonInfo():string {
+//     return `${this.userName} ${this.surName} ${this.year}`
+// }
+// }
+
+// const myPerson = new Person('YELAMAN', 'JANIBEKOV', 2020);
+// console.log(myPerson.getPersonInfo())
